f136664bb4776179f5e0e02096468ad3de511e3b
kersseliM <ttk14smarkoj@kamk.fi>
2016-03-17 14:06:10 +0200

    BombScene

---
+++ LinnanJamit2016/Assets/Bomb.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour
+{
+
+    BoxCollider bc;
+    // Use this for initialization
+    void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Explode();
+
+        }
+    }
+
+    private void Explode()
+    {
+        bc.enabled = true;
+    }
+}
---
+++ LinnanJamit2016/Assets/CellMaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellMaster : MonoBehaviour
+{
+    public GameObject main, pieces;
+
+    void Start()
+    {
+        pieces.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Active();
+        }
+
+    }
+
+
+
+    void Active()
+    {
+        main.SetActive(false);
+        pieces.SetActive(true);
+    }
+
+}
