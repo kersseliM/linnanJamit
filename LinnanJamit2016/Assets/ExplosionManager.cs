0c252028d0c3901c21d40632de02ddceef3210a5
kersseliM <ttk14smarkoj@kamk.fi>
2016-03-17 18:31:34 +0200

    fgfggf

--- LinnanJamit2016/Assets/ExplosionManager.cs
+++ LinnanJamit2016/Assets/ExplosionManager.cs
@@ -28,3 +28,5 @@ public class ExplosionManager : MonoBehaviour
         b.Explode();
     }
 }
+
+
--- LinnanJamit2016/Assets/Text_Effect.cs
+++ LinnanJamit2016/Assets/Text_Effect.cs
@@ -4,18 +4,47 @@ using System.Collections;
 public class Text_Effect : MonoBehaviour
 {
     TextMesh textMEsh;
-
+    Vector3 posToLerp;
+    Vector3 startPos;
+    float timeToLERP = 1;
+    float timer;
 
     // Use this for initialization
     void Awake()
     {
         textMEsh = GetComponent<TextMesh>();
+        SetText("tedsad");
     }
 
 
     public void SetText(string _text)
     {
         textMEsh.text = _text;
+        lerp = true;
+        posToLerp = transform.position + Vector3.up*2;
+        startPos = transform.position;
+    
     }
+
+
+    void Update()
+    {
+
+        if(lerp)
+        {
+            timer +=Time.deltaTime;
+
+            float percentage = timer / timeToLERP;
+            transform.position = Vector3.Lerp(startPos, posToLerp, percentage);
+            if(percentage>= 1)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    bool lerp = false;
+
 
 }
